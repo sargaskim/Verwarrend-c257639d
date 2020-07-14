97e5ead8b80d0344a38f9bad68d4430ec25fa483
sargaskim <2103702@talnet.nl>
2020-07-14 11:10:12 +0200

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -8,12 +8,10 @@ naam varchar(10) NOT NULL,
 diameter int(11) NOT NULL,
 afstand BIGINT(225) NOT NULL, 
 massa float(11) NOT NULL,
-bezoek_datum YEAR
+bezoek_datum YEAR,
+id MEDIUMINT NOT NULL AUTO_INCREMENT,
+PRIMARY KEY (id)
 );
-
-alter table planeten 
-add id MEDIUMINT NOT NULL AUTO_INCREMENT,
-add PRIMARY KEY (id);
 
 INSERT INTO planeten (naam, diameter, afstand, massa)
 	VALUES('Zon', '1392000',	'0', '332946');
@@ -31,15 +29,22 @@ INSERT INTO planeten (naam, diameter, afstand, massa)
 -- mars 2
 INSERT INTO planeten (naam, diameter, afstand, massa)
 	VALUES('Mars','6794','227936640','0.1');
-INSERT INTO planeten (naam, diameter, afstand, massa)
-	VALUES('Jupiter','142984','778412010','318');
-INSERT INTO planeten (naam, diameter, afstand, massa)
-	VALUES('Saturnus',	'120536', '1426725400',	'95');
-INSERT INTO planeten (naam, diameter, afstand, massa)
-	VALUES('Uranus', '51118',	'2870972200', '1');
-INSERT INTO planeten (naam, diameter, afstand, massa)
-	VALUES('Neptunus',	'49572', '4498252900', '17');
+-- INSERT INTO planeten (naam, diameter, afstand, massa)
+-- 	VALUES('Jupiter','142984','778412010','318');
+-- INSERT INTO planeten (naam, diameter, afstand, massa)
+-- 	VALUES('Saturnus',	'120536', '1426725400',	'95');
+-- INSERT INTO planeten (naam, diameter, afstand, massa)
+-- 	VALUES('Uranus', '51118',	'2870972200', '1');
+-- INSERT INTO planeten (naam, diameter, afstand, massa)
+-- 	VALUES('Neptunus',	'49572', '4498252900', '17');
 
+select naam
+from planeten
+where id = 7;
+    
+update planeten
+set naam = 'Teenalp'
+where id = 7;
 
 SELECT * FROM planeten;
 
